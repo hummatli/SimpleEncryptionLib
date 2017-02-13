75671f0751174da73db4f4280e8597df55cbc4bc
Sattar Hummatli <settarxan@gmail.com>
2017-02-13 14:11:37 +0400

    Added fork me github button

--- MAHEncryptorLib/mah-encryptor-android-sample/src/main/java/com/mobapphome/mahencryptorlib/sample/MainActivity.java
+++ MAHEncryptorLib/mah-encryptor-android-sample/src/main/java/com/mobapphome/mahencryptorlib/sample/MainActivity.java
@@ -1,11 +1,16 @@
 package com.mobapphome.mahencryptorlib.sample;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.mobapphome.mahencryptorlib.Constants;
 import com.mobapphome.mahencryptorlib.MAHEncryptor;
 import com.mobapphome.mahencryptorlib.R;
 
@@ -19,67 +24,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextKeyPhrase = (EditText) findViewById(R.id.etKeyPhrase);
+        editTextOriginal = (EditText) findViewById(R.id.etOriginalTxt);
+        editTextEnchrypted = (EditText) findViewById(R.id.etEnchryptedTxt);
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.ivMAHForkMeOnGithub);
+        Drawable forkMeImg = getResources().getDrawable(R.drawable.forkme_green);
+        // setting the opacity (alpha)
+        forkMeImg.setAlpha(180);
+        // setting the images on the ImageViews
+        imageView.setImageDrawable(forkMeImg);
+
+        imageView.setOnClickListener(this);
         findViewById(R.id.btnDechrypt).setOnClickListener(this);
         findViewById(R.id.btnEnchrypt).setOnClickListener(this);
-        editTextKeyPhrase = (EditText)findViewById(R.id.etKeyPhrase);
-        editTextOriginal = (EditText)findViewById(R.id.etOriginalTxt);
-        editTextEnchrypted = (EditText)findViewById(R.id.etEnchryptedTxt);
     }
 
     @Override
     public void onClick(View view) {
-        String keyPhraseTxt = editTextKeyPhrase.getText().toString().trim();
-        if (keyPhraseTxt.isEmpty()) {
-            new AlertDialog.Builder(this)
-                    .setTitle("Caution!")
-                    .setMessage("Key phrase is empty. Fill it and continue.")
-                    .setPositiveButton("Ok", null)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-            return;
-        }
 
-        try {
+        if (view.getId() == R.id.ivMAHForkMeOnGithub) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.MAH_ENCHRIPTOR_GITHUB_LINK));
+            startActivity(browserIntent);
+        } else if (view.getId() == R.id.btnEnchrypt
+                || view.getId() == R.id.btnDechrypt) {
 
-            if (view.getId() == R.id.btnEnchrypt) {
-                MAHEncryptor mahEncryptor = MAHEncryptor.newInstance(keyPhraseTxt);
-
-                String originaltxt = editTextOriginal.getText().toString().trim();
-                if (originaltxt.isEmpty()){
+            try {
+                String keyPhraseTxt = editTextKeyPhrase.getText().toString().trim();
+                if (keyPhraseTxt.isEmpty()) {
                     new AlertDialog.Builder(this)
                             .setTitle("Caution!")
-                            .setMessage("Original string is empty. Fill it and continue.")
+                            .setMessage("Key phrase is empty. Fill it and continue.")
                             .setPositiveButton("Ok", null)
                             .setIcon(android.R.drawable.ic_dialog_alert)
                             .show();
                     return;
                 }
-                editTextEnchrypted.setText(mahEncryptor.encode(originaltxt));
 
-            } else if (view.getId() == R.id.btnDechrypt) {
-                MAHEncryptor mahEncryptor = MAHEncryptor.newInstance(keyPhraseTxt);
+                if (view.getId() == R.id.btnEnchrypt) {
+                    MAHEncryptor mahEncryptor = MAHEncryptor.newInstance(keyPhraseTxt);
 
-                String enchryptedTxt = editTextEnchrypted.getText().toString().trim();
-                if (enchryptedTxt.isEmpty()){
-                    new AlertDialog.Builder(this)
-                            .setTitle("Caution!")
-                            .setMessage("Enchrypted string is empty. Fill it and continue.")
-                            .setPositiveButton("Ok", null)
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .show();
-                    return;
+                    String originaltxt = editTextOriginal.getText().toString().trim();
+                    if (originaltxt.isEmpty()) {
+                        new AlertDialog.Builder(this)
+                                .setTitle("Caution!")
+                                .setMessage("Original string is empty. Fill it and continue.")
+                                .setPositiveButton("Ok", null)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                        return;
+                    }
+                    editTextEnchrypted.setText(mahEncryptor.encode(originaltxt));
+
+                } else if (view.getId() == R.id.btnDechrypt) {
+                    MAHEncryptor mahEncryptor = MAHEncryptor.newInstance(keyPhraseTxt);
+
+                    String enchryptedTxt = editTextEnchrypted.getText().toString().trim();
+                    if (enchryptedTxt.isEmpty()) {
+                        new AlertDialog.Builder(this)
+                                .setTitle("Caution!")
+                                .setMessage("Enchrypted string is empty. Fill it and continue.")
+                                .setPositiveButton("Ok", null)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                        return;
+                    }
+                    editTextOriginal.setText(mahEncryptor.decode(enchryptedTxt));
                 }
-                editTextOriginal.setText(mahEncryptor.decode(enchryptedTxt));
+            } catch (Exception e) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Exception")
+                        .setMessage(e.getMessage())
+                        .setPositiveButton("Ok", null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+                return;
             }
-        }catch (Exception e){
-            new AlertDialog.Builder(this)
-                    .setTitle("Exception")
-                    .setMessage(e.getMessage())
-                    .setPositiveButton("Ok", null)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-            return;
         }
-
     }
 }
---
+++ MAHEncryptorLib/mah-encryptor-lib/src/main/java/com/mobapphome/mahencryptorlib/Constants.java
@@ -0,0 +1,10 @@
+package com.mobapphome.mahencryptorlib;
+
+/**
+ * Created by settar on 2/6/17.
+ */
+
+public class Constants {
+    public static final String MAH_ENCHRIPTOR_GITHUB_LINK = "https://github.com/hummatli/MAHEncryptorLib";
+
+}
